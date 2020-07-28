58ade271636176639d4c17f6f0259c584ed1398f
flora.zhang <flora.zhang@rccchina.com>
2020-07-28 18:05:16 +0800

    init todo app commit

---
+++ todo-list/src/components/Avatar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="avater">
+    <div class="avater_face">
+      <img src="/img/face.ab84b391.jpeg" alt />
+    </div>
+    <h2 class="avater_name">Hello Jane</h2>
+    <p class="avater_tip">
+      Looks like feed good.
+      <br />You have 6 tasks to do today.
+    </p>
+  </div>
+</template>
---
+++ todo-list/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <app-bar :title="'TODO'" />
+    <avatar />
+  </div>
+</template>
+<script>
+import AppBar from "./../components/AppBar.vue";
+import Avatar from "./../components/Avatar.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppBar,
+    Avatar
+  }
+};
+</script>
